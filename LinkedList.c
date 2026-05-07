c7364245e306f65aaa4b5f4ac4327bf271ddb7da
Qhroal12 <kfc030306@gmail.com>
2026-05-07 10:44:20 +0900

    Update LinkedList.c

--- LinkedList.c
+++ LinkedList.c
@@ -83,11 +83,13 @@ LinkedList* deleteAtLinkedList(LinkedList* li, int at) {
 			PointType* fPtr = li->head;
 			li->head = fPtr->next;
 			free(fPtr);
+			li->size--;
 		}
 		else {
 			PointType* fPtr = nPtr->next;
 			nPtr->next = fPtr->next;
 			free(fPtr);
+			li->size--;
 		}
 	}
 }
